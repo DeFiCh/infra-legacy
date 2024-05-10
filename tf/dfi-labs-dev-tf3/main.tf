e88807c938ce5f501cc6e9b92d72a9c5670e66ae
Prasanna Loganathar <pvl@prasannavl.com>
2024-05-10 13:23:28 +0800

    Turn az into arrays

--- tf/dfi-labs-dev-tf3/main.tf
+++ tf/dfi-labs-dev-tf3/main.tf
@@ -11,12 +11,14 @@ locals {
   tf_service_account = "tf-service-account@${local.project_id}.iam.gserviceaccount.com"
 
   az = {
-    asia = { region : "asia-southeast1", zone : "asia-southeast1-a" },
-    eu   = { region : "europe-west1", zone : "europe-west1-b" },
-    us   = { region : "us-central1", zone : "us-central1-a" },
+    asia = [{ region: "asia-southeast1", zone: "asia-southeast1-a" }],
+    eu   = [{ region: "europe-west1", zone: "europe-west1-b" }],
+    us   = [{ region: "us-central1", zone: "us-central1-a" }],
   }
-  default_region = local.az.asia.region
-  default_zone   = local.az.asia.zone
+
+  default_region = local.az.asia[0].region
+  default_zone   = local.az.asia[0].zone
+
   services = [
     "cloudresourcemanager.googleapis.com",
     "compute.googleapis.com",
